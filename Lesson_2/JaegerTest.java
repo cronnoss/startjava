00bd13ad91f14b02d7f7f67023d343993ca35e4f
Evgeniy Karasev <karasevevg@gmail.com>
2019-03-02 19:09:27 +1000

    the order is changed - first 1 object, then 2 object

--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -1,49 +1,61 @@
 public class JaegerTest {
 
     public static void main(String[] args) {
-        Jaeger jaegerOne = new Jaeger();
-        Jaeger jaegerTwo = new Jaeger();
+        // создание 1-го экземпляра класса Jaeger
+        Jaeger gipsyDanger = new Jaeger();
         
         // присваивание значений полям объекта wolfOne с помощью сеттеров:
-        jaegerOne.setModelName("Gipsy Danger");
-        jaegerOne.setMark("Mark-3");
-        jaegerOne.setOrigin("USA");
-        jaegerOne.setWeight(1.980f);
-        jaegerOne.setSpeed(7);
-
-        jaegerTwo.setModelName("Cherno Alpha");
-        jaegerTwo.setMark("Mark-1");
-        jaegerTwo.setOrigin("Russia");
-        jaegerTwo.setWeight(2.412f);
-        jaegerTwo.setSpeed(3);
+        gipsyDanger.setModelName("Gipsy Danger");
+        gipsyDanger.setMark("Mark-3");
+        gipsyDanger.setOrigin("USA");
+        gipsyDanger.setWeight(1.980f);
+        gipsyDanger.setSpeed(7);
 
         // считывание значений полей объекта с помощью геттеров:
-        System.out.println("Jaeger's name is " + jaegerOne.getModelName());
-        System.out.println("Jaeger's mark is " + jaegerOne.getMark());
-        System.out.println("Jaeger's origin is " + jaegerOne.getOrigin());
-        System.out.println("Jaeger's weight is " + jaegerOne.getWeight());
-        System.out.println("Jaeger's speed is " + jaegerOne.getSpeed());
-
-        System.out.println("Jaeger's name is " + jaegerTwo.getModelName());
-        System.out.println("Jaeger's mark is " + jaegerTwo.getMark());
-        System.out.println("Jaeger's origin is " + jaegerTwo.getOrigin());
-        System.out.println("Jaeger's weight is " + jaegerTwo.getWeight());
-        System.out.println("Jaeger's speed is " + jaegerTwo.getSpeed());
+        System.out.println("Jaeger's name is " + gipsyDanger.getModelName());
+        System.out.println("Jaeger's mark is " + gipsyDanger.getMark());
+        System.out.println("Jaeger's origin is " + gipsyDanger.getOrigin());
+        System.out.println("Jaeger's weight is " + gipsyDanger.getWeight());
+        System.out.println("Jaeger's speed is " + gipsyDanger.getSpeed());
 
         // перезаписывание и вывод значения полей
-        jaegerOne.setMark("java");
-        System.out.println("Jaeger's mark is " + jaegerOne.getMark());
-        jaegerOne.setSpeed(700);
-        System.out.println("Jaeger's speed is " + jaegerOne.getSpeed());
-
-        jaegerTwo.setOrigin("It's modified!");
-        System.out.println("Jaeger's origin is " + jaegerTwo.getOrigin());
-        jaegerTwo.setWeight(0.001f);
-        System.out.println("Jaeger's weight is " + jaegerTwo.getWeight());
+        gipsyDanger.setMark("java");
+        System.out.println("Jaeger's mark is " + gipsyDanger.getMark());
+        gipsyDanger.setSpeed(700);
+        System.out.println("Jaeger's speed is " + gipsyDanger.getSpeed());
 
         // вызов метода объекта:
-        jaegerOne.drift();
-        jaegerOne.move();
-        System.out.println(jaegerOne.scanKaiju());
+        gipsyDanger.drift();
+        gipsyDanger.move();
+        System.out.println(gipsyDanger.scanKaiju());
+        
+        
+        // создание 2-го экземпляра класса Jaeger
+        Jaeger chernoAlpha = new Jaeger();
+        
+        // присваивание значений полям объекта wolfOne с помощью сеттеров:
+        chernoAlpha.setModelName("Cherno Alpha");
+        chernoAlpha.setMark("Mark-1");
+        chernoAlpha.setOrigin("Russia");
+        chernoAlpha.setWeight(2.412f);
+        chernoAlpha.setSpeed(3);
+
+        // считывание значений полей объекта с помощью геттеров:
+        System.out.println("Jaeger's name is " + chernoAlpha.getModelName());
+        System.out.println("Jaeger's mark is " + chernoAlpha.getMark());
+        System.out.println("Jaeger's origin is " + chernoAlpha.getOrigin());
+        System.out.println("Jaeger's weight is " + chernoAlpha.getWeight());
+        System.out.println("Jaeger's speed is " + chernoAlpha.getSpeed());
+
+        // перезаписывание и вывод значения полей
+        chernoAlpha.setOrigin("It's modified!");
+        System.out.println("Jaeger's origin is " + chernoAlpha.getOrigin());
+        chernoAlpha.setWeight(0.001f);
+        System.out.println("Jaeger's weight is " + chernoAlpha.getWeight());
+
+        // вызов метода объекта:
+        chernoAlpha.drift();
+        chernoAlpha.move();
+        System.out.println(chernoAlpha.scanKaiju());
     }
 }
