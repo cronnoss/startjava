db6b9d898772077d3be4afd98f6075c88802c230
Evgeniy Karasev <karasevevg@gmail.com>
2019-03-19 14:57:18 +1000

    input of numbers and sign made in the class CalculatorTest

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -6,9 +6,58 @@ public class CalculatorTest {
         String answer = "да";
         do {
             Calculator calculatorOne = new Calculator();
+            
             calculatorOne.inputX();
-            calculatorOne.inputY();
+            System.out.print("Введите первое число: ");
+            Scanner scanX = new Scanner(System.in);
+            calculatorOne.setX(scanX.nextLong());
+                while(calculatorOne.getX() <= 0) {
+                    System.out.print("Калькулятор выполняет математические операции над целыми положительными числами. Пожалуйста введите положительное число: ");
+                    calculatorOne.setX(scanX.nextLong());
+                }
+
             calculatorOne.calculate();
+            do {
+                System.out.print("Введите знак математической операции: ");
+                Scanner scanSign = new Scanner(System.in);
+                calculatorOne.setSign(scanSign.nextLine());
+    
+                calculatorOne.inputY();
+                System.out.print("Введите второе число: ");
+                Scanner scanY = new Scanner(System.in);
+                calculatorOne.setY(scanY.nextLong());
+                while(calculatorOne.getY() <= 0) {
+                    System.out.print("Калькулятор выполняет математические операции над целыми положительными числами. Пожалуйста введите положительное число: ");
+                    calculatorOne.setY(scanY.nextLong());
+                    }
+
+                switch (calculatorOne.getSign()) {
+                    case "+" : 
+                        calculatorOne.setResult(calculatorOne.getX() + calculatorOne.getY());
+                        break;
+                    case "-" : 
+                        calculatorOne.setResult(calculatorOne.getX() - calculatorOne.getY());
+                        break;
+                    case "*" : 
+                        calculatorOne.setResult(calculatorOne.getX() * calculatorOne.getY());
+                        break;
+                    case "/" : 
+                        calculatorOne.setResult(calculatorOne.getX() / calculatorOne.getY());
+                        break; 
+                    case "^" : 
+                        calculatorOne.setResult(1);
+                        for(long i = 1; i <= calculatorOne.getY(); i++) {
+                            calculatorOne.setResult(calculatorOne.getResult() * calculatorOne.getX());
+                        }
+                        break;
+                    case "%" : 
+                        calculatorOne.setResult(calculatorOne.getX() % calculatorOne.getY());
+                        break;
+                    default : System.out.println("Введите одну из следующих математических операции: +, -, *, /, ^, % ");
+                        break;
+                }
+            } while(!calculatorOne.getSign().equals("+") && !calculatorOne.getSign().equals("-") && !calculatorOne.getSign().equals("*") && !calculatorOne.getSign().equals("/") && !calculatorOne.getSign().equals("^") && !calculatorOne.getSign().equals("%"));
+
             System.out.println(calculatorOne.getX() + " " + calculatorOne.getSign() + " " + calculatorOne.getY() + " = " + calculatorOne.getResult());
 
             Scanner scanAnswer = new Scanner(System.in);
