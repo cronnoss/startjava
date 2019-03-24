068fd28fe7c7b04c701a4cdc480d296b725275f0
Evgeniy Karasev <karasevevg@gmail.com>
2019-03-24 15:12:38 +1000

    reordered. First check input, then set variable

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -3,28 +3,25 @@ import java.util.Scanner;
 public class CalculatorTest {
 
     public static void main(String[] args) {
-        Calculator calculatorOne = new Calculator();
+        System.out.println("Калькулятор выполняет математические операции (+, -, *, /, ^, %) над целыми положительными числами.");
         Scanner scan = new Scanner(System.in);
-
         String answer = "да";
         
-        while (answer.equals("да")) {
-            System.out.print("Введите первое число: ");
-            calculatorOne.setX(scan.nextLong());
-                while(calculatorOne.getX() <= 0) {
-                    System.out.print("Пожалуйста введите целое положительное число: ");
-                    calculatorOne.setX(scan.nextLong());
-                }
+        while(answer.equals("да")) {
+            Calculator calculatorOne = new Calculator();
+
+            while(calculatorOne.getX() <= 0) {
+                System.out.print("Введите первое число: ");
+                calculatorOne.setX(scan.nextLong());
+            }
             
             System.out.print("Введите знак математической операции: ");
             calculatorOne.setSign(scan.next());
 
-            System.out.print("Введите второе число: ");
-            calculatorOne.setY(scan.nextLong());
             while(calculatorOne.getY() <= 0) {
-                System.out.print("Пожалуйста введите целое положительное число: ");
+                System.out.print("Введите второе число: ");
                 calculatorOne.setY(scan.nextLong());
-                }
+            }
 
             calculatorOne.calculate();
 
